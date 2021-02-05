6b0938daefe687020de16a77b61d023a9d3da6f8
jie3.liu <jie3.liu@zhangmen.com>
2021-02-05 17:47:40 +0800

    feat: 配置www文件

--- routes/ws.js
+++ routes/ws.js
@@ -5,12 +5,12 @@ var router = express.Router();
 expressWs(router);
 console.log(222, router)
 router.ws('/userinfo',function(ws, req){
-    ws.send("你连接成功了");
     ws.on('message', function (msg) {
         // 业务代码
         console.log('链接上了', msg)
         ws.send("这是第二次发送信息");
     })
+    ws.send("你连接成功了");
 })
 
 module.exports = router;
