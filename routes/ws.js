b57854d6102f7c77ec94f4aa37bb051f30d594f9
jie3.liu <jie3.liu@zhangmen.com>
2021-02-24 19:50:00 +0800

    feat:完善

--- routes/ws.js
+++ routes/ws.js
@@ -4,13 +4,28 @@ var expressWs = require('express-ws'); // websocket
 var router = express.Router();
 expressWs(router);
 console.log(222, router)
-router.ws('/userinfo',function(ws, req){
-    ws.on('message', function (msg) {
-        // 业务代码
-        console.log('链接上了', msg)
-        ws.send("这是第二次发送信息");
-    })
-    ws.send("你连接成功了");
+router.ws('/userinfo', function (ws, req) {
+  ws.on('message', function (msg) {
+    ws.send("这是第二次发送信息");
+    let flag = 0;
+    let count = 0;
+    if (flag === 0) {
+      let intervalTimer = setInterval(() => {
+        let str = JSON.stringify({title: "这是定时器发送的信息", count: count})
+        ws.send(str);
+        if (count > 100) {
+          clearInterval(intervalTimer)
+        };
+        count++;
+      }, 2000);
+    }
+    flag++;
+  })
+  ws.on('close', function(msg, event){
+    console.log( '====',msg, event)
+  })
+  ws.send("你连接成功了");
+  
 })
 
 module.exports = router;
